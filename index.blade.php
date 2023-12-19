bfc83a276d0eef7c82b5f6b13235cfa51c4ebac3
umarrr-22 <136910065+umarrr-22@users.noreply.github.com>
2023-12-20 00:08:39 +0700

    Add files via upload

--- index.blade.php
+++ index.blade.php
@@ -1,58 +1,55 @@
 <x-app-layout>
     <x-slot name="header">
-        
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Pinjam') }}
+            {{ __('Prodi') }}
         </h2>
     </x-slot>
-    <head>
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
-        <link rel="stylesheet" href="style.css">
-    </head>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="mb-6">
-                <a href="{{ url('/pinjam/create') }}" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
-                    + Create data 
+            <div class="mb-10">
+                <a href="{{ url('/prodi/create') }}" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+                    + Create Prodi
                 </a>
             </div>
 
-            <div class="bg-white overflow-x-auto">
-                <table class="min-w-full border-collapse">
+            <div class="bg-white">
+                <table class="table-auto w-full">
                     <thead>
                         <tr>
-                            <th class="border px-4 py-2">Anggota</th>
-                            <th class="border px-4 py-2">Buku</th>
-                            <th class="border px-4 py-2">Tanggal pinjam</th>
-                            <th class="border px-4 py-2">Tanggal kembali</th>
-                          
-                            <th class="border px-4 py-2">Actions</th> 
+                            <th class="border px-6 py-4">Fakultas</th>
+                            <th class="border px-6 py-4">Prodi</th>
+                            <th class="border px-6 py-4">Action</th>
                         </tr>
                     </thead>
                     <tbody>
-                        @forelse($items as $item)
+                        @forelse($prodi as $item)
                         <tr>
-                            <td class="border px-4 py-2">{{ $item->id_anggota }}</td>
-                            <td class="border px-4 py-2">{{ $item->id_buku }}</td>
-                            <td class="border px-4 py-2">{{ $item->tgl_pinjam }}</td>
-                            <td class="border px-4 py-2">{{ $item->tgl_kembali }}</td>
-                           
-                            <td class="border px-2 py-2 text-center" id="tombol">
-    <a href="{{ url('/pinjam/edit/'.$item->id_pinjam) }}" class="inline-block bg-blue-500 hover:bg-blue-400 text-white font-bold py-1 px-2 mx-1 rounded">
-        <i class="fas fa-pencil-alt"></i>
-    </a>
-    <form action="{{ url('/pinjam/destroy/'.$item->id_pinjam) }}" method="POST" class="inline-block">
-        @csrf
-        <button type="submit" class="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 mx-1 rounded inline-block" onclick="return confirm('Are you sure?')">
-            <i class="fas fa-trash-alt"></i>
-        </button>
-    </form>
-</td>
+                            <td class="border px-6 py-4">
+                                @if($item->fakultas)
+                                    {{ $item->fakultas->nama_fakultas }}
+                                @else
+                                    Fakultas Tidak Diketahui
+                                @endif
+                            </td>
+                            <td class="border px-6 py-4">
+                                {{ $item->nama_prodi }}
+                            </td>
+                            <td class="border px-6 py- text-center">
+                                <a href="{{ url('/prodi/edit/'.$item->id) }}" class="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mx-2 rounded">
+                                    Edit
+                                </a>
+                                <form action="{{ url('/prodi/destroy/'.$item->id) }}" method="POST" class="inline-block">
+                                    @csrf
+                                    <button type="submit" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 mx-2 rounded inline-block" onclick="return confirm('Are you sure?')">
+                                        Delete
+                                    </button>
+                                </form>
+                            </td>
                         </tr>
                         @empty
                         <tr>
-                            <td colspan="6" class="border text-center p-5">
+                            <td colspan="3" class="border text-center p-5">
                                 Data Tidak Ditemukan
                             </td>
                         </tr>
@@ -62,7 +59,7 @@
             </div>
 
             <div class="text-center mt-5">
-                {{ $items->links() }}
+                {{ $prodi->links() }}
             </div>
         </div>
     </div>
